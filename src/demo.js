e6032830f583c9b621de4f596852bd544e1c5d31
fizban007 <fizban007@gmail.com>
2024-04-11 10:00:18 -0500

    added resistance

--- src/demo.js
+++ src/demo.js
@@ -49,6 +49,7 @@ var Config = function () {
   this.dphi2dt = 0.0;
   this.g = 9.81;
   this.dt = 0.02;
+  this.resistance = 0.0;
   this.run = false;
   this.running = false;
   this.reset_camera = function() {
@@ -81,7 +82,7 @@ rod1.rotateX(Math.PI/2);
 pivot1.add(rod1);
 var mass1 = new THREE.Mesh(new THREE.SphereGeometry(1.0, 32, 32),
                            new THREE.MeshPhongMaterial({ color: 0xdd0000 }));
-mass1.scale.set(conf.m1, conf.m1, conf.m1);
+mass1.scale.set(conf.m1**(1/3), conf.m1**(1/3), conf.m1**(1/3));
 mass1.translateZ(-conf.L1);
 pivot1.add(mass1);
 var pivot2 = new THREE.Group();
@@ -97,7 +98,7 @@ pivot2.add(rod2);
 
 var mass2 = new THREE.Mesh(new THREE.SphereGeometry(1.0, 32, 32),
                            new THREE.MeshPhongMaterial({ color: 0x00dd00 }));
-mass2.scale.set(conf.m2, conf.m2, conf.m2);
+mass2.scale.set(conf.m2**(1/3), conf.m2**(1/3), conf.m2**(1/3));
 mass2.translateZ(-conf.L2);
 pivot2.add(mass2);
 
@@ -109,8 +110,9 @@ gui.add(conf, 'phi1', -2.0*Math.PI, 2.0*Math.PI).name('phi1').listen();
 gui.add(conf, 'phi2', -2.0*Math.PI, 2.0*Math.PI).name('phi2').listen();
 gui.add(conf, 'L1', 1.0, 20.0).name('L1').listen();
 gui.add(conf, 'L2', 1.0, 20.0).name('L2').listen();
-gui.add(conf, 'm1', 0.1, 5.0).name('m1').listen();
-gui.add(conf, 'm2', 0.1, 5.0).name('m2').listen();
+gui.add(conf, 'm1', 0.01, 10.0, 0.01).name('m1').listen();
+gui.add(conf, 'm2', 0.01, 10.0, 0.01).name('m2').listen();
+gui.add(conf, 'resistance', 0.0, 0.5, 0.01).name('resistance').listen();
 gui.add(conf, 'dt', 0.001, 0.1, 0.001).name('dt').listen();
 gui.add(conf, 'reset_camera').name('Reset Camera');
 
@@ -127,6 +129,7 @@ var derives = function(x, y) {
   var L1 = conf.L1;
   var L2 = conf.L2;
   var g = conf.g;
+  var b = conf.resistance;
 
   var sin_dphi = Math.sin(phi2-phi1);
   var cos_dphi = Math.cos(phi2-phi1);
@@ -137,12 +140,12 @@ var derives = function(x, y) {
                   m2 * g * sin_phi2 * cos_dphi +
                   m2 * L2 * phi2dot * phi2dot * sin_dphi -
                   (m1 + m2) * g * sin_phi1)
-    / (L1 * (m1 + m2 * sin_dphi * sin_dphi));
+    / (L1 * (m1 + m2 * sin_dphi * sin_dphi)) - b * phi1dot;
 
   var phi2ddot = (-m2 * L2 * phi2dot * phi2dot * sin_dphi * cos_dphi -
                   L1 * (m1 + m2) * phi1dot * phi1dot * sin_dphi +
                   (m1 + m2) * g * (sin_phi1 * cos_dphi - sin_phi2))
-    / (L2 * (m1 + m2 * sin_dphi * sin_dphi));
+    / (L2 * (m1 + m2 * sin_dphi * sin_dphi)) - b * phi2dot;
 
   return [phi1dot, phi1ddot, phi2dot, phi2ddot];
 }
@@ -167,12 +170,12 @@ function animate() {
   rod1.scale.set(1.0, conf.L1, 1.0);
   // mass1.position.y = conf.L1 * Math.sin(conf.phi1);
   mass1.position.z = -conf.L1;
-  mass1.scale.set(conf.m1, conf.m1, conf.m1);
+  mass1.scale.set(conf.m1**(1/3), conf.m1**(1/3), conf.m1**(1/3));
   pivot2.position.z = -conf.L1;
   // rod1.translateY(-conf.L1/2);
   rod2.scale.set(1.0, conf.L2, 1.0);
   mass2.position.z = -conf.L2;
-  mass2.scale.set(conf.m2, conf.m2, conf.m2);
+  mass2.scale.set(conf.m2**(1/3), conf.m2**(1/3), conf.m2**(1/3));
 
   pivot1.setRotationFromAxisAngle(new THREE.Vector3(1.0, 0.0, 0.0), conf.phi1);
   pivot2.setRotationFromAxisAngle(new THREE.Vector3(1.0, 0.0, 0.0), conf.phi2 - conf.phi1);
